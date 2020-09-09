0337aa1b4f490884597373aea9eb6a8dacc92b31
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2020-09-09 08:25:46 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World!!!!!!, HAVE A GREAT DAY !3!3  php-II version is " . PHP_VERSION . "\n";
+print "Hello, World!!!!!!, HAVE A GREAT DAY !4!4  php-I version is " . PHP_VERSION . "\n";
 ?>
